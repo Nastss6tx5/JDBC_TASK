a19a9e12f1d483deafc0fa407b3b3d57628abf44
Anastasiia <nastasiia227@gmail.com>
2025-01-22 14:57:56 +0300

    import fixed

--- src/main/java/vladdossik/jdbc/jpa/dao/UserDaoJDBCImpl.java
+++ src/main/java/vladdossik/jdbc/jpa/dao/UserDaoJDBCImpl.java
@@ -2,7 +2,11 @@ package vladdossik.jdbc.jpa.dao;
 
 import vladdossik.jdbc.jpa.model.User;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
